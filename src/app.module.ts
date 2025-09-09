00beaea0552170bd96f77c181cafdff558cc47c5
julien-pla <julien.c.pla@gmail.com>
2025-09-09 14:56:16 +0200

    Création Structure Projet

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FormationModule } from './formation/formation.module';
+import { GroupeModule } from './groupe/groupe.module';
+import { StudentModule } from './student/student.module';
+import { TeamMemberModule } from './team-member/team-member.module';
 
 @Module({
-  imports: [],
+  imports: [FormationModule, GroupeModule, StudentModule, TeamMemberModule],
   controllers: [AppController],
   providers: [AppService],
 })
